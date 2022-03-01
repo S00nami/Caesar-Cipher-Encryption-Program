e64a2616962b557b947e846b2a7b6c6787bf79c2
HTN <96134288+htn-dev@users.noreply.github.com>
2022-03-01 09:59:04 -0500

    Update caesar-cipher.py

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -5,7 +5,7 @@ def getDoubleAlphabet(alphabet):
 def getMessage():
     stringToEncrypt = input("Please enter a message to encrypt: ")
     return  stringToEncrypt
-   
+  
 def getCipherKey():
     shiftAmount = input("Please enter a key (whole number from 1-25): ")
     return shiftAmount
