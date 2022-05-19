8bda88fb13c59d713494d51711205ae978424280
HTN <96134288+htn-dev@users.noreply.github.com>
2022-05-19 13:11:57 -0400

    Update caesar-cipher.py

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,7 +1,7 @@
 def getDoubleAlphabet(alphabet):
     doubleAlphabet = alphabet + alphabet
     return doubleAlphabet
-   
+  
 def getMessage():
     stringToEncrypt = input("Please enter a message to encrypt: ")
     return  stringToEncrypt
